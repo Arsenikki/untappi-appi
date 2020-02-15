1a677b055fd2b76272220bb95a18dbd1a3531ff4
Arsenikki <42617129+Arsenikki@users.noreply.github.com>
2020-02-15 18:47:07 +0200

    Add beerController DB actions

--- backend-tappi/Controllers/BeerController.cs
+++ backend-tappi/Controllers/BeerController.cs
@@ -15,17 +15,21 @@ namespace backend_tappi.Controllers
     [Route("[controller]")]
     public class BeerController : ControllerBase
     {
-        private readonly IConfiguration _config;
         private readonly ILogger<BeerController> _logger;
-        private MySqlConnection connection;
+        private string _connectionString;
+        private string _apiUrl;
+        private string _clientIdSecret;
+        private MySqlConnection _connection;
 
         public BeerController(ILogger<BeerController> logger, IConfiguration config)
         {
             _logger = logger;
-            _config = config;
+            _connectionString = config.GetValue<string>("DB_CONNECTION");
+            _apiUrl = config.GetValue<string>("API_URL");
+            _clientIdSecret = config.GetValue<string>("CLIENT_ID_SECRET");
         }
 
-        // GET: /Beer/5
+        // GET: /Beer/189889
         [HttpGet("{venueId}", Name = "GetBeers")]
         public async Task<List<ParsedBeer>> GetAsync(int venueId)
         {
@@ -50,55 +54,55 @@ namespace backend_tappi.Controllers
             return beersInVenue;
         }
 
-        private Task FetchBeersForVenue(int venueId)
+        private async Task<List<ParsedBeer>> FetchBeersForVenue(int venueId)
         {
-            throw new NotImplementedException();
+            var listOfBeers = new List<ParsedBeer>();
+            return listOfBeers;
+
             // Fetch combined table
         }
 
         private async Task AddBeersToDatabase(List<ParsedBeer> beersInVenue)
         {
-            connection = new MySqlConnection(_config.GetValue<string>("DB_CONNECTION"));
+            _connection = new MySqlConnection(_connectionString);
+            await _connection.OpenAsync();
             _logger.LogInformation("Establishing connection to SQL DB");
- 
-            beersInVenue.ForEach(async beer =>
+
+            beersInVenue.ForEach(beer =>
             {
                 try
                 {
-                    await InsertBeer(beer);
+                    InsertBeer(beer);
                 }
                 catch (Exception ex)
                 {
-                    _logger.LogError(ex.Message);
+                    _logger.LogError("InsertBeer failed: ", ex.Message, ex.InnerException.Message);
                 }
             });
 
-            await connection.CloseAsync();
+            await _connection.CloseAsync();
         }
 
-        private async Task InsertBeer(ParsedBeer beer)
+        private void InsertBeer(ParsedBeer beer)
         {
-            await connection.OpenAsync();
-
             MySqlCommand cmd = new MySqlCommand();
-            cmd.Connection = connection;
+            cmd.Connection = _connection;
             cmd.CommandText = @"INSERT INTO beers (name, brewery, country, style, stronkness, rating) VALUES (@name, @brewery, @country, @style, @stronkness, @rating);";
             cmd.Prepare();
+            cmd.Parameters.AddWithValue("@name", beer.Id);
             cmd.Parameters.AddWithValue("@name", beer.Name);
             cmd.Parameters.AddWithValue("@brewery", beer.Brewery);
             cmd.Parameters.AddWithValue("@country", beer.Country);
             cmd.Parameters.AddWithValue("@style", beer.Style);
             cmd.Parameters.AddWithValue("@stronkness", beer.Stronkness);
             cmd.Parameters.AddWithValue("@rating", beer.Rating);
-            int rowCount = await cmd.ExecuteNonQueryAsync();
-            Console.WriteLine(String.Format("Number of rows inserted={0}", rowCount));
+            int rowCount = cmd.ExecuteNonQuery();
+            Console.WriteLine(String.Format("Number of beers inserted: {0}", rowCount));
         }
 
         private async Task<List<ParsedBeer>> GetBeersFromAPI(int venueId)
         {
-            string apiUrl = _config.GetValue<string>("API_URL");
-            string clientIdSecret = _config.GetValue<string>("CLIENT_ID_SECRET");
-            string request = apiUrl + "venue/info/" + venueId + "?" + clientIdSecret;
+            string request = _apiUrl + "venue/info/" + venueId + "?" + _clientIdSecret;
             RootObject payloadObject = await ExecuteAPIRequest(request);
 
             // Start parsing the output object
@@ -115,7 +119,8 @@ namespace backend_tappi.Controllers
                     Country = items[i].brewery.country_name,
                     Style = items[i].beer.beer_style,
                     Rating = items[i].beer.rating_score,
-                    Stronkness = items[i].beer.beer_abv
+                    Stronkness = items[i].beer.beer_abv,
+                    Id = items[i].beer.bid
                 });
                 listOfBeers.Add(beer);
 			}
