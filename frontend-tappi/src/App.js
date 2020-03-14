21f2a4d6a07c0e14b4e9708d667a1d4db6f2eeda
Arsenikki <42617129+Arsenikki@users.noreply.github.com>
2020-03-14 15:29:19 +0200

    change URL + run linter

--- frontend-tappi/src/App.js
+++ frontend-tappi/src/App.js
@@ -1,130 +1,148 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import React, { useState, useEffect } from 'react';
-import MapView from './components/MapView';
-import BeerList from './components/BeerList';
-import styled from 'styled-components';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect } from "react";
+import MapView from "./components/MapView";
+import BeerList from "./components/BeerList";
+import styled from "styled-components";
 
 const App = () => {
-    const [myLocation, setMyLocation] = useState({ lat: null, lng: null });
-    const [venueLocations, setVenueLocations] = useState([]);
-    const [beersInVenues, setBeersInVenue] = useState([]);
+  const [myLocation, setMyLocation] = useState({ lat: null, lng: null });
+  const [venueLocations, setVenueLocations] = useState([]);
+  const [beersInVenues, setBeersInVenue] = useState([]);
 
-    const [venuesLoadedBool, setVenuesLoadedBool] = useState(false);
-    const [beersLoadedBool, setBeersLoadedBool] = useState(false);
-    
-    const [selectedVenue, setSelectedVenue] = useState([]);
-    const [selectedBeers, setSelectedBeers] = useState([]);
+  const [venuesLoadedBool, setVenuesLoadedBool] = useState(false);
+  const [beersLoadedBool, setBeersLoadedBool] = useState(false);
 
-    useEffect(() => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(geoLocation => handleMyLocationChange(geoLocation))
-        } else {
-            console.log("Your browser doesn't support HTML5 :S")
-        }
-    }, [])
+  const [selectedVenue, setSelectedVenue] = useState([]);
+  const [selectedBeers, setSelectedBeers] = useState([]);
 
-    useEffect(() => {
-        if (myLocation.lat !== null && myLocation.lng !== null) {
-            populateVenueData()
-        }
-    }, [myLocation])
-
-    useEffect(() => {
-        if (venueLocations.length > 0) {
-            populateBeerData()
-        }
-    }, [venueLocations])
-
-    useEffect(() => {
-        if (selectedVenue !== null && selectedVenue[0] !== null && selectedVenue[1] !== null) {
-            giveSelectedVenueBeers()
-        }
-    }, [selectedVenue])
-
-    const handleMyLocationChange = (geoLocation) => {
-        const { latitude, longitude } = geoLocation.coords
-        setMyLocation({ lat: latitude, lng: longitude })
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(geoLocation =>
+        handleMyLocationChange(geoLocation)
+      );
+    } else {
+      console.log("Your browser doesn't support HTML5 :S");
     }
+  }, []);
 
-    const handleVenueSelection = (event) => {
-        setSelectedVenue([ event.target.options.position.venueName, event.target.options.position.venueID ])
+  useEffect(() => {
+    if (myLocation.lat !== null && myLocation.lng !== null) {
+      populateVenueData();
     }
+  }, [myLocation]);
 
-    const populateVenueData = async () => {
-        const response = await fetch(`http://localhost:32769/venue/${myLocation.lat}&${myLocation.lng}`);
-        const data = await response.json();
-        console.log("tas saatu venue daatta", data);
-        setVenueLocations(data);
-        setVenuesLoadedBool(true);
+  useEffect(() => {
+    if (venueLocations.length > 0) {
+      populateBeerData();
     }
+  }, [venueLocations]);
 
-    const populateBeerData = async () => {
-        let allBeers = await Promise.all(
-            venueLocations.map(async venue => {
-                const beerResponse = await fetch(`http://localhost:32769/beer/${venue.venueID}`);
-                const json = beerResponse.json()
-                return json
-            })
-        )
-
-        for (var i = 0; i < venueLocations.length; i++) {
-            allBeers[i].unshift({ name: venueLocations[i].venueName, id: venueLocations[i].venueID })
-        }
-        console.log(allBeers)
-        setBeersInVenue(allBeers);
-        setBeersLoadedBool(true);
+  useEffect(() => {
+    if (
+      selectedVenue !== null &&
+      selectedVenue[0] !== null &&
+      selectedVenue[1] !== null
+    ) {
+      giveSelectedVenueBeers();
     }
+  }, [selectedVenue]);
 
-    const giveSelectedVenueBeers = () => {
-        for (var i = 0; i < beersInVenues.length; i++) {
-            const venueNameIdObject = beersInVenues[i][0]
-            if (Object.values(venueNameIdObject).includes(selectedVenue[0])) {
-                setSelectedBeers(beersInVenues[i])
-            }
-        }
-    }
+  const handleMyLocationChange = geoLocation => {
+    const { latitude, longitude } = geoLocation.coords;
+    setMyLocation({ lat: latitude, lng: longitude });
+  };
 
-    const StyledMap = styled.div`    
-        width: 100%;
-        height: 100%;
-        position: absolute;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 1;
-    `
+  const handleVenueSelection = event => {
+    setSelectedVenue([
+      event.target.options.position.venueName,
+      event.target.options.position.venueID
+    ]);
+  };
 
-    const StyledBeersOverlap = styled.div`
-        width: 100%;
-        height: 20%;
-        z-index: 2;
-    `
-
-    const StyledContainer = styled.div`
-        display: flex;
-        alignItems: center;
-        justifyContent: center;
-    `
-
-    return (
-        <StyledContainer>
-            <StyledMap>
-                <MapView
-                    myLocation={myLocation}
-                    venueLocations={venueLocations}
-                    locationsLoaded={venuesLoadedBool}
-                    handleVenueSelection={handleVenueSelection}
-                />
-            </StyledMap>
-            <StyledBeersOverlap>
-                {beersLoadedBool
-                    ? <BeerList selectedBeers={selectedBeers} selectedVenue={selectedVenue} />
-                    : null
-                }
-            </StyledBeersOverlap>
-        </StyledContainer>
+  const populateVenueData = async () => {
+    const response = await fetch(
+      `https://untappiappi.westeurope.cloudapp.azure.com/venue/${myLocation.lat}&${myLocation.lng}`
     );
-}
+    const data = await response.json();
+    console.log("tas saatu venue daatta", data);
+    setVenueLocations(data);
+    setVenuesLoadedBool(true);
+  };
+
+  const populateBeerData = async () => {
+    let allBeers = await Promise.all(
+      venueLocations.map(async venue => {
+        const beerResponse = await fetch(
+          `https://untappiappi.westeurope.cloudapp.azure.com/beer/${venue.venueID}`
+        );
+        const json = beerResponse.json();
+        return json;
+      })
+    );
+
+    for (var i = 0; i < venueLocations.length; i++) {
+      allBeers[i].unshift({
+        name: venueLocations[i].venueName,
+        id: venueLocations[i].venueID
+      });
+    }
+    console.log(allBeers);
+    setBeersInVenue(allBeers);
+    setBeersLoadedBool(true);
+  };
+
+  const giveSelectedVenueBeers = () => {
+    for (var i = 0; i < beersInVenues.length; i++) {
+      const venueNameIdObject = beersInVenues[i][0];
+      if (Object.values(venueNameIdObject).includes(selectedVenue[0])) {
+        setSelectedBeers(beersInVenues[i]);
+      }
+    }
+  };
+
+  const StyledMap = styled.div`
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 1;
+  `;
+
+  const StyledBeersOverlap = styled.div`
+    width: 100%;
+    height: 20%;
+    z-index: 2;
+  `;
+
+  const StyledContainer = styled.div`
+    display: flex;
+    alignitems: center;
+    justifycontent: center;
+  `;
+
+  return (
+    <StyledContainer>
+      <StyledMap>
+        <MapView
+          myLocation={myLocation}
+          venueLocations={venueLocations}
+          locationsLoaded={venuesLoadedBool}
+          handleVenueSelection={handleVenueSelection}
+        />
+      </StyledMap>
+      <StyledBeersOverlap>
+        {beersLoadedBool ? (
+          <BeerList
+            selectedBeers={selectedBeers}
+            selectedVenue={selectedVenue}
+          />
+        ) : null}
+      </StyledBeersOverlap>
+    </StyledContainer>
+  );
+};
 
 export default App;
--- frontend-tappi/src/components/MapView.js
+++ frontend-tappi/src/components/MapView.js
@@ -1,61 +1,66 @@
-import 'leaflet/dist/leaflet.css';
-import React from 'react';
-import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-import Leaflet from 'leaflet'
-import { popupContent, popupHead } from '../popupStyles';
+import "leaflet/dist/leaflet.css";
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import Leaflet from "leaflet";
+import { popupContent, popupHead } from "../popupStyles";
 
-import student from '../icons/student.svg';
-import beer from '../icons/beer.svg'
+import student from "../icons/student.svg";
+import beer from "../icons/beer.svg";
 
 let studentIcon = Leaflet.icon({
-    iconUrl: student,
-    iconSize: [35, 35],
-    iconAnchor: [12, 36],
-    popupAnchor: [0, -25]
+  iconUrl: student,
+  iconSize: [35, 35],
+  iconAnchor: [12, 36],
+  popupAnchor: [0, -25]
 });
 
 let BeerIcon = Leaflet.icon({
-    iconUrl: beer,
-    iconSize: [35, 35],
-    iconAnchor: [16, 32]
+  iconUrl: beer,
+  iconSize: [35, 35],
+  iconAnchor: [16, 32]
 });
 
-const MapView = ({ myLocation, venueLocations, locationsLoaded, handleVenueSelection }) => {
-    return (
-        <Map style={{ "height": "100%" }} center={myLocation} zoom={14}>
-            <TileLayer
-                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {myLocation ?
-                <Marker icon={studentIcon} position={myLocation}>
-                <Popup className="request-popup">
-                    <div style={popupContent}>
-                        <img
-                        src={studentIcon}
-                        alt="Large person icon"
-                        width="150"
-                        height="150"
-                        />
-                        <div style={popupHead}>
-                        I am you
-                        </div>
-                    </div>
-                </Popup>
-                </Marker>
-                : null
-            }
-            {locationsLoaded
-                ? venueLocations.map(venue => {
-                    console.log("venueee", venue)
-                    return (
-                        <Marker icon={BeerIcon} key={venue.venueName} position={venue} onClick={handleVenueSelection} />
-                    )
-                })
-                : null
-            }
-        </Map>
-    )
-}
+const MapView = ({
+  myLocation,
+  venueLocations,
+  locationsLoaded,
+  handleVenueSelection
+}) => {
+  return (
+    <Map style={{ height: "100%" }} center={myLocation} zoom={14}>
+      <TileLayer
+        attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      {myLocation ? (
+        <Marker icon={studentIcon} position={myLocation}>
+          <Popup className="request-popup">
+            <div style={popupContent}>
+              <img
+                src={studentIcon}
+                alt="Large person icon"
+                width="150"
+                height="150"
+              />
+              <div style={popupHead}>I am you</div>
+            </div>
+          </Popup>
+        </Marker>
+      ) : null}
+      {locationsLoaded
+        ? venueLocations.map(venue => {
+            return (
+              <Marker
+                icon={BeerIcon}
+                key={venue.venueName}
+                position={venue}
+                onClick={handleVenueSelection}
+              />
+            );
+          })
+        : null}
+    </Map>
+  );
+};
 
 export default MapView;
